86393288695908a3344d7364046a496267bff259
Crescencio-Francisco <116220482+Crescencio-Francisco@users.noreply.github.com>
2024-08-01 19:22:13 +0200

    up

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('yes-btn').addEventListener('click', function() {
+    alert("Sabia que você diria sim! 😊");
+});
+
+document.getElementById('of-course-btn').addEventListener('click', function() {
+    alert("Você é a melhor! ❤️");
+});
